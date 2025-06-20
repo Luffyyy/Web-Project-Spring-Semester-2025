b2820d473f0f1aefac904104f8289bb3328c156d
Luffy <luffydafloffi@gmail.com>
2025-06-20 11:03:55 +0300

    Add thumbnail class

--- components/list-exercise.js
+++ components/list-exercise.js
@@ -3,7 +3,7 @@ import { capitalize } from "@/lib/utils";
 export default function ListExercise({ exercise, children, preContent, content }) {
     return <div className="content exercise" >
         {preContent}
-        <img src={exercise.thumbnail} width="150" alt={`${exercise.title} thumbnail`} />
+        <img src={exercise.thumbnail} className="thumbnail self-center" alt={`${exercise.title} thumbnail`} />
         <div className="flex flex-col">
             <strong className="text-2xl">{exercise.title}</strong>
             <span className="text-left">{capitalize(exercise.difficulty)}</span>
