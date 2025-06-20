b88940a3aeea19c026fdfaae228eb04706fb4ded
Luffy <luffydafloffi@gmail.com>
2025-06-20 11:05:52 +0300

    Fix user context improperly being provided

--- components/layout/client-layout.js
+++ components/layout/client-layout.js
@@ -1,7 +1,6 @@
 "use client"
-import { createContext, useEffect, useRef, useState } from "react";
+import { createContext, useState } from "react";
 import Header from "./header";
-import { useCookies } from "next-client-cookies";
 
 export const ThemeContext = createContext();
 export const UserContext = createContext();
@@ -10,7 +9,7 @@ export default function ClientLayout({ theme, user: initialUser, children }) {
     const [ user, setUser ] = useState(initialUser);
 
     return <ThemeContext.Provider value={theme}>
-        <UserContext value={{user, setUser}}>
+        <UserContext.Provider value={{user, setUser}}>
             <Header theme={theme}/>
 
             <main className="grow flex flex-col items-center w-full">
@@ -20,6 +19,6 @@ export default function ClientLayout({ theme, user: initialUser, children }) {
             <footer className="p-4 content rounded z-10">
                 <span>Made By Group 2 - Braude College</span>
             </footer>
-        </UserContext>
+        </UserContext.Provider>
     </ThemeContext.Provider>
 }
